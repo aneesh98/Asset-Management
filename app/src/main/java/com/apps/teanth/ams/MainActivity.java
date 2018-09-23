68ccb337b4a247f48e26f3d6039c8abe4c6935f7
Tejas <teanth98@gmail.com>
2018-09-23 22:57:07 +0530

    mysql->android

--- app/src/main/java/com/apps/teanth/ams/MainActivity.java
+++ app/src/main/java/com/apps/teanth/ams/MainActivity.java
@@ -4,9 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     List<users> userList;
@@ -40,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //adding some items to our list
-        userList.add(
+        /*userList.add(
                 new users(
                         "teanth98@gmail.com",
                         "Tejas",
@@ -59,7 +72,51 @@ public class MainActivity extends AppCompatActivity {
                         "2016.tejas.thakur@ves.ac.in",
                         "xyz",
                         "DPO"
-                ));
+                ));*/
+        String url=URL.url+"userlist";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Data",response);
+                        try
+                        { JSONArray jsonArray=new JSONArray(response);
+                          for(int j=0;j<jsonArray.length();j++)
+                          {
+                              JSONObject UserObj=jsonArray.getJSONObject(j);
+                              String email=UserObj.getString("email");
+                              String name=UserObj.getString("name");
+                              String post="DPO";
+                              userList.add(new users(email,name,post));
+                          }
+                        }
+                        catch(JSONException e)
+                        {
+                            Log.e("Json Error","Something Went Wrong");
+                        }
+
+
+
+                    }
+
+
+                }
+
+                , new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("Error","Something Went Wrong");
+        }
+    }){
+        @Override
+        protected Map<String,String> getParams() throws AuthFailureError {
+
+            Map<String,String> params=new HashMap<String, String>();
+
+            return params;
+        }
+    };
+        MySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
 
         //creating recyclerview adapter
         userAdapter adapter = new userAdapter(this, userList);
---
+++ app/src/main/java/com/apps/teanth/ams/URL.java
@@ -0,0 +1,5 @@
+package com.apps.teanth.ams;
+
+public class URL {
+    static String url="http://192.168.1.2:8012/public/api/";
+}
